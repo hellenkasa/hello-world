0ff4386cba6dc8daddea9dd466d29e99bd1eea4d
Hellen K <hellenk205@gmail.com>
2021-07-14 23:13:25 +0530

    second commit

--- hello.js
+++ hello.js
@@ -1,3 +1,14 @@
 // New console 
 
-console.log("Hello World");
+// console.log("Hello World");
+
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+console.log("5");
+console.log("6");
+console.log("")
+console.log("")
+console.log("")
+console.log("")
